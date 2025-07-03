1aa7c1f54106dc63adaec04f0ee023419b709e35
unknown <sofiane.draris@ynov.com>
2025-07-03 15:06:30 +0200

    Fix terraform variables and GitHub Actions env for Azure SP auth

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -4,24 +4,5 @@ variable "postgres_admin_password" {
   sensitive   = true
 }
 
-variable "client_id" {
-  description = "Azure client ID"
-  type        = string
-}
 
-variable "client_secret" {
-  description = "Azure client secret"
-  type        = string
-  sensitive   = true
-}
-
-variable "tenant_id" {
-  description = "Azure tenant ID"
-  type        = string
-}
-
-variable "subscription_id" {
-  description = "Azure subscription ID"
-  type        = string
-}
 
