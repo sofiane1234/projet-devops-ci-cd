69c8ad483ca9af338b0546a90ca6cf554f53703d
unknown <sofiane.draris@ynov.com>
2025-07-03 19:53:17 +0200

    Flake8 me casse la tete

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/ping")
 def ping():
     return {"message": "pong"}
+
--- backend/tests/test_main.py
+++ backend/tests/test_main.py
@@ -3,7 +3,9 @@ from app.main import app
 
 client = TestClient(app)
 
+
 def test_ping():
     response = client.get("/ping")
     assert response.status_code == 200
     assert response.json() == {"message": "pong"}
+
