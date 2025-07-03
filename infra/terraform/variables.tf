add81074e1a9b0960101a42db514afa1a1a2b77a
unknown <sofiane.draris@ynov.com>
2025-07-03 16:08:02 +0200

    Come on terraform plan

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -4,5 +4,23 @@ variable "postgres_admin_password" {
   sensitive   = true
 }
 
+variable "subscription_id" {
+  type        = string
+  description = "Azure Subscription ID"
+}
 
+variable "client_id" {
+  type        = string
+  description = "Azure Client ID"
+}
 
+variable "client_secret" {
+  type        = string
+  description = "Azure Client Secret"
+  sensitive   = true
+}
+
+variable "tenant_id" {
+  type        = string
+  description = "Azure Tenant ID"
+}
