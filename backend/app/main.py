a1d6bfa9320ee2d30c3ba097ce34d3d7bf3ce8d1
unknown <sofiane.draris@ynov.com>
2025-07-06 16:56:27 +0200

    Ajout route /health-db pour test PostgreSQL

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,37 +1,41 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 import psycopg2
+import os
 
 app = FastAPI()
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_methods=["*"],
-    allow_headers=["*"]
-)
-
-
 def connect():
     return psycopg2.connect(
-        host="pg-sosso-devops-ci-cd.postgres.database.azure.com",
-        database="employeesdb",
-        user="adminuser@pg-sosso-devops-ci-cd",
-        password="SuperSecretPassword123"
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT", 5432),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        connect_timeout=5
     )
-
-
-@app.get("/employees")
-def get_employees():
-    conn = connect()
-    cur = conn.cursor()
-    cur.execute("SELECT id, name, role FROM employees;")
-    rows = cur.fetchall()
-    cur.close()
-    conn.close()
-    return [{"id": r[0], "name": r[1], "role": r[2]} for r in rows]
-
 
 @app.get("/")
 def root():
     return {"message": "Bienvenue dans l'API employé"}
+
+@app.get("/health-db")
+def health_db():
+    try:
+        conn = connect()
+        conn.close()
+        return {"status": "ok"}
+    except Exception as e:
+        return {"status": "error", "detail": str(e)}
+
+@app.get("/employees")
+def get_employees():
+    try:
+        conn = connect()
+        cur = conn.cursor()
+        cur.execute("SELECT id, name, role FROM employees;")
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+        return [{"id": r[0], "name": r[1], "role": r[2]} for r in rows]
+    except Exception as e:
+        return {"error": str(e)}
